106ac8aba9ac07492246167d39fa0833004f724f
Cameron Legleiter <camlegleiter@gmail.com>
2017-02-25 08:59:07 -0600

    Apply NOT NULL property to all table columns

--- migrations/20170219074803_setup_games_table.js
+++ migrations/20170219074803_setup_games_table.js
@@ -1,14 +1,14 @@
 exports.up = (knex) => Promise.all([
   knex.schema.createTable('games', (t) => {
-    t.uuid('id').primary().unique();
-    t.string('wordLength');
-    t.string('lettersGuessed');
-    t.integer('remainingIncorrectGuesses');
-    t.boolean('isGameOver');
-    t.boolean('isWinner');
-    t.string('word');
-    t.timestamp('createdOn').defaultTo(knex.fn.now());
-    t.timestamp('updatedOn').defaultTo(knex.fn.now());
+    t.uuid('id').primary().unique().notNull();
+    t.string('wordLength').notNull();
+    t.string('lettersGuessed').notNull();
+    t.integer('remainingIncorrectGuesses').notNull();
+    t.boolean('isGameOver').notNull();
+    t.boolean('isWinner').notNull();
+    t.string('word').notNull();
+    t.timestamp('createdOn').notNull().defaultTo(knex.fn.now());
+    t.timestamp('updatedOn').notNull().defaultTo(knex.fn.now());
   }),
 ]);
 
